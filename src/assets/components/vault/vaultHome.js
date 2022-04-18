6fb7ed14232efe7b4c058570429b7963b5e23332
Arnab Chaudhuri <arnab15102012@gmail.com>
2022-04-18 21:55:05 +0530

    vault_wip

--- src/assets/components/vault/vaultHome.js
+++ src/assets/components/vault/vaultHome.js
@@ -1,6 +1,6 @@
 import cx from "classnames";
 import React from "react";
-import { BsCameraVideo} from 'react-icons/bs';
+import { BsChevronRight} from 'react-icons/bs';
 import { FiPhone } from 'react-icons/fi';
 import styles from "./../../scss/design.module.scss";
 
@@ -12,20 +12,22 @@ function VaultFavourites() {
     {id: 1, type: 'folder', name: 'Design curriculum', poster: 'https://picsum.photos/390'},
     {id: 1, type: 'doc', name: 'PRD requirements', poster: 'https://picsum.photos/400'},
     {id: 1, type: 'folder', name: 'PRD requirements', poster: 'https://picsum.photos/410'},
-    {id: 1, type: 'doc', name: 'PRD requirements', poster: 'https://picsum.photos/420'}
   ];
 
   const vaultCard = (data) => {
     return (
       <div className={styles.vaultCard}>
-
+        <img className={styles.vaultCardPreview} src="https://prodjam-assets.s3.ap-south-1.amazonaws.com/doc_preview.png"/>
       </div>
     )
   };
 
   return (
     <div>
-      <div className={cx(styles.sectionHeader)}>Favourites</div>
+      <div className={cx(styles.flex_row_layout, styles.mb_14)}>
+        <div className={styles.section_title}>Favourites</div>
+        <div className={styles.flex_row_layout}>View All<BsChevronRight className={styles.ml_8}/></div>
+      </div>
       <div className={styles.fileRow}>
         {
           favItems.map((item, index) => {
--- src/pages/Vault.js
+++ src/pages/Vault.js
@@ -2,12 +2,13 @@ import React, {  useState, useEffect, useRef, memo } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom';
 import cx from 'classnames';
 import styles from '../assets/scss/design.module.scss';
+import VaultFavourites from '../assets/components/vault/vaultHome';
 
 function Vault() {
             
     return (
         <div className={styles.right_content_container}>
-            Vault
+            <div><VaultFavourites/></div>
         </div>
     )
 }
