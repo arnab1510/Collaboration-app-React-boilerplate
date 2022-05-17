0612f48fd231169376bf4fdb91ca26eeb5ede693
Arnab Chaudhuri <arnab15102012@gmail.com>
2022-05-17 22:40:54 +0530

    ui

--- src/assets/components/vault/vaultCards.js
+++ src/assets/components/vault/vaultCards.js
@@ -4,23 +4,50 @@ import { BsChevronRight} from 'react-icons/bs';
 import { FiMoreVertical } from 'react-icons/fi';
 import styles from "./../../scss/design.module.scss";
 import DocIcon from "../../images/doc_icon.svg";
+import FolderIcon from "../../images/folder_icon.svg";
+import FolderPreview from "../../images/folder_preview.png";
 
-function VaultCards() {
+const cardPreview = (type) => {
+  if (type==="doc") {
+    return (
+      <div className={cx(styles.padding_12, styles.text_center)}>
+        <img className={styles.vaultCardPreview} src="https://prodjam-assets.s3.ap-south-1.amazonaws.com/doc_preview.png"/>
+      </div>
+    )
+  }
+  else if (type==="folder") {
+    return (
+      <div className={cx(styles.padding_12, styles.folderPreview)}>
+        <img className={cx(styles.vaultCardPreview)} src={FolderPreview}/>
+      </div>
+    )
+  }
+}
 
-  const vaultCard = (data) => {
+const prodLogo = (type) => {
+  if (type==="doc") {
+    return (
+      DocIcon
+    )
+  }
+  else if (type==="folder") {
+    return (
+      FolderIcon
+    )
+  }
+}
+
+export const vaultCard = (data) => {
     return (
       <div className={styles.vaultCard}>
-        <div className={styles.padding_12}>
-          <div className={styles.menuDots}><FiMoreVertical className={styles.cardMenu}/></div>
-          <img className={styles.vaultCardPreview} src="https://prodjam-assets.s3.ap-south-1.amazonaws.com/doc_preview.png"/>
+        <div className={styles.menuDots}>
+          <span><FiMoreVertical className={styles.cardMenu}/></span>
         </div>
+        {cardPreview(data.type)}
         <div className={cx(styles.bottom_section)}>
-          <img className={styles.file_icon} src={DocIcon}/>
+          <img className={styles.file_icon} src={prodLogo(data.type)}/>
           <span>{data.name}</span>
         </div>
       </div>
     )
   };
-}
-
-export default VaultCards;
--- src/assets/components/vault/vaultHome.js
+++ src/assets/components/vault/vaultHome.js
@@ -4,6 +4,7 @@ import { BsChevronRight} from 'react-icons/bs';
 import { FiMoreVertical } from 'react-icons/fi';
 import styles from "./../../scss/design.module.scss";
 import DocIcon from "../../images/doc_icon.svg";
+import { vaultCard } from "./vaultCards";
 
 function VaultFavourites() {
 
@@ -27,21 +28,6 @@ function VaultFavourites() {
       }
     }
   }
-
-  const vaultCard = (data) => {
-    return (
-      <div className={styles.vaultCard}>
-        <div className={styles.padding_12}>
-          <div className={styles.menuDots}><FiMoreVertical className={styles.cardMenu}/></div>
-          <img className={styles.vaultCardPreview} src="https://prodjam-assets.s3.ap-south-1.amazonaws.com/doc_preview.png"/>
-        </div>
-        <div className={cx(styles.bottom_section)}>
-          <img className={styles.file_icon} src={DocIcon}/>
-          <span>{data.name}</span>
-        </div>
-      </div>
-    )
-  };
 
   return (
     <div>
