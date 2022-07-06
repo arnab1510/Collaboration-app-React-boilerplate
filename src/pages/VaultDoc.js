a9fa3075f7948e23fd4581d07204406f09db9a88
Arnab Chaudhuri <arnab15102012@gmail.com>
2022-07-06 20:03:38 +0530

    ui

--- src/pages/VaultDoc.js
+++ src/pages/VaultDoc.js
@@ -1,10 +1,93 @@
 import styles from '../assets/scss/design.module.scss';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import "quill-mention";
+import Picker from 'emoji-picker-react';
+import { Input } from 'antd';
+import cx from "classnames";
+import { useRef, useState } from 'react';
+import { saveAs } from 'file-saver';
+import * as quillToWord from 'quill-to-word';
 
 function VaultDoc() {
-            
+
+    const [value, setValue] = useState('');
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+    const quillRef = useRef();
+
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+    };
+    
+    const values = [
+        { 
+            id: "515fd775-cb54-41f3-b921-56163871e2cf", 
+            value: "Mickey Dooley" 
+        },
+        {
+            id: "3f0b7933-57b8-4d9d-b238-f8af62b2e945",
+            value: "Desmond Waterstone"
+        },
+        { 
+            id: "711f68ab-ca20-4011-ab0f-d98c8fac4c05", 
+            value: "Jeralee Fryd"
+        },
+        { 
+            id: "775e05fc-72bc-48a1-9508-5c61674734f1",
+            value: "Eddie Hucquart"
+        },
+        { 
+            id: "e8701885-105e-4a21-b200-98e559776655", 
+            value: "Nathalia Whear"
+        }
+    ];
+
+    VaultDoc.formats = [
+        'bold', 'italic', 'underline', 'strike',
+        'list', 'bullet', 'indent',
+        'link', 'color', 'code'
+    ];
+
+    VaultDoc.modules = {
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike','code'],
+          [{'list': 'ordered'}, {'list': 'bullet'}, 
+           {'indent': '-1'}, {'indent': '+1'}],
+          ['link'],
+          [{'color': ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color']}]
+        ],
+    clipboard: {
+        matchVisual: false,
+        }
+    };
+
+
+    const downloadDoc = () => {
+        const editor = quillRef.getContents();
+        // const unprivilegedEditor = quillRef.makeUnprivilegedEditor(editor);
+        // const contents = unprivilegedEditor.getContents();
+        const blob = quillToWord.generateWord(editor,{exportAs: "blob"});
+        saveAs(blob, "word-export.docx");
+    };
+    
     return (
-        <div className={styles.right_content_container}>
-            dfgdg
+        <div>
+            <div className={cx(styles.doc_container, 'doc_container')}>
+                <div className={cx(styles.docHeader_top)}>
+                    <Input placeholder="New document" bordered={false} />
+                </div>
+                <div>
+                    <ReactQuill ref={quillRef} theme="snow" value={value} onChange={setValue}
+                        placeholder="Type your message here"
+                        formats={VaultDoc.formats}
+                        modules={VaultDoc.modules}
+                    />
+                </div>
+                <div>
+                    <button onClick={() => downloadDoc()}>Download</button>
+                </div>
+            </div>
         </div>
     )
 }
