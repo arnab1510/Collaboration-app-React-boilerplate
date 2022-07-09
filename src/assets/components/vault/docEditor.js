335a66276d45c9b19722a3e9e56ec8d30832d3dc
Arnab Chaudhuri <arnab15102012@gmail.com>
2022-07-09 22:09:53 +0530

    docEditor

--- src/assets/components/vault/docEditor.js
+++ src/assets/components/vault/docEditor.js
@@ -2,15 +2,54 @@ import cx from "classnames";
 import React, { useEffect } from "react";
 import { BsChevronRight} from 'react-icons/bs';
 import styles from "./../../scss/design.module.scss";
-import { EditorContent, useEditor } from '@tiptap/react'
+import { EditorContent, useEditor, BubbleMenu } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import DocMenuBar from "./docMenuBar";
+import FloatingMenu from '@tiptap/extension-floating-menu'
+import Underline from '@tiptap/extension-underline';
+import Highlight from '@tiptap/extension-highlight';
+import TextStyle from '@tiptap/extension-text-style'
+import { Color } from '@tiptap/extension-color';
+import Mention from '@tiptap/extension-mention';
+import Link from '@tiptap/extension-link';
+import Table from '@tiptap/extension-table';
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+import TextAlign from '@tiptap/extension-text-align';
 
 function DocEditor(props) {
 
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit, Underline, TextStyle, Color, 
+      // FloatingMenu.configure({element: document.querySelector('.menu')})
+      Highlight.configure({ multicolor: true }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        linkOnPaste: true
+      }),
+      Mention.configure({
+        HTMLAttributes: {
+          class: 'mention',
+        },
+        // suggestion,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+      })
     ],
     content: ``,
   });
@@ -21,6 +60,48 @@ function DocEditor(props) {
 
   return (
     <div className={styles.editorPage}>
+      {editor && <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          bold
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+          italic
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        >
+          strike
+        </button>
+      </BubbleMenu>}
+
+      {/* {editor && <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+        >
+          h1
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+        >
+          h2
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          bullet list
+        </button>
+      </FloatingMenu>} */}
+
       <EditorContent editor={editor} />
     </div>
   )
--- src/assets/components/vault/docMenuBar.js
+++ src/assets/components/vault/docMenuBar.js
@@ -1,18 +1,39 @@
 import cx from "classnames";
-import React from "react";
+import React, { useCallback } from "react";
 import { BsTypeBold, BsCodeSlash, BsTypeUnderline } from 'react-icons/bs';
 import { FiItalic } from 'react-icons/fi';
-import { AiOutlineStrikethrough } from 'react-icons/ai';
-import { GrOrderedList } from 'react-icons/gr';
+import { AiOutlineStrikethrough, AiOutlineLine } from 'react-icons/ai';
+import { GrOrderedList, GrUndo, GrRedo } from 'react-icons/gr';
 import { MdFormatListBulleted } from 'react-icons/md';
 import { Select } from 'antd';
-
 import styles from "./../../scss/design.module.scss";
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
 const { Option } = Select;
 const DocMenuBar = ({ editor }) => {
+
+  const setLink = useCallback(() => {
+    const previousUrl = editor.getAttributes('link').href
+    const url = window.prompt('URL', previousUrl)
+
+    // cancelled
+    if (url === null) {
+      return
+    }
+
+    // empty
+    if (url === '') {
+      editor.chain().focus().extendMarkRange('link').unsetLink()
+        .run()
+
+      return
+    }
+
+    // update link
+    editor.chain().focus().extendMarkRange('link').setLink({ href: url })
+      .run()
+  }, [editor]);
 
   if (!editor) {
     return null
@@ -22,44 +43,82 @@ const DocMenuBar = ({ editor }) => {
 
   };
 
+  const returnIcon = (name, icon, func) => {
+    return (
+      <div onClick={func}
+        className={cx(editor.isActive(name) ? 'is-active' : '', styles.iconFlex)}>
+        {icon}
+      </div>
+    );
+  };
+
   return (
     <div className={styles.menuToolbar}>
-      <div
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive('bold') ? 'is-active' : ''}
+      {returnIcon('bold', <BsTypeBold/>, () => editor.chain().focus().toggleBold().run())}
+      {returnIcon('italic', <FiItalic/>, () => editor.chain().focus().toggleItalic().run())}
+      {returnIcon('underline', <BsTypeUnderline/>, () => editor.chain().focus().toggleUnderline().run())}
+      {returnIcon('strike', <AiOutlineStrikethrough/>, () => editor.chain().focus().toggleStrike().run())}
+      {returnIcon('code', <BsCodeSlash/>, () => editor.chain().focus().toggleCode().run())}
+      {returnIcon('bulletList', <MdFormatListBulleted/>, () => editor.chain().focus().toggleBulletList().run())}
+      {returnIcon('orderedList', <GrOrderedList/>, () => editor.chain().focus().toggleOrderedList().run())}
+      {returnIcon('codeBlock', <BsCodeSlash/>, () => editor.chain().focus().toggleCodeBlock().run())}
+      {returnIcon('horRule', <AiOutlineLine/>, () => editor.chain().focus().setHorizontalRule().run())}
+      {returnIcon('undo', <GrUndo/>, () => editor.chain().focus().undo().run())}
+      {returnIcon('redo', <GrRedo/>, () => editor.chain().focus().redo().run())}
+
+      <button
+        onClick={() => editor.chain().focus().toggleHighlight({ color: '#ffc078' }).run()}
+        className={editor.isActive('highlight', { color: '#ffc078' }) ? 'is-active' : ''}
       >
-        <BsTypeBold/>
-      </div>
-      <div
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={editor.isActive('italic') ? 'is-active' : ''}
+        orange
+      </button>
+
+      <button
+        onClick={() => editor.chain().focus().toggleTaskList().run()}
+        className={editor.isActive('taskList') ? 'is-active' : ''}
       >
-        <FiItalic/>
-      </div>
-      <div
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        className={editor.isActive('strike') ? 'is-active' : ''}
+        toggleTaskList
+      </button>
+
+      <button onClick={setLink} className={editor.isActive('link') ? 'is-active' : ''}>
+        setLink
+      </button>
+
+
+      <button
+        onClick={() => editor.chain().focus().setTextAlign('left').run()}
+        className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}
       >
-        <AiOutlineStrikethrough/>
-      </div>
-      <div
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        className={editor.isActive('code') ? 'is-active' : ''}
+        left
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign('center').run()}
+        className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}
       >
-        <BsCodeSlash/>
-      </div>
-      {/* <div
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive('paragraph') ? 'is-active' : ''}
+        center
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign('right').run()}
+        className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}
       >
-        paragraph
-      </div> */}
-      <div
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive('paragraph') ? 'is-active' : ''}
+        right
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign('justify').run()}
+        className={editor.isActive({ textAlign: 'justify' }) ? 'is-active' : ''}
       >
-        <BsTypeUnderline/>
-      </div>
+        justify
+      </button>
+      <button onClick={() => editor.chain().focus().unsetTextAlign().run()}>unsetTextAlign</button>
+
+      
+
+      <input
+        type="color"
+        onInput={event => editor.chain().focus().setColor(event.target.value).run()}
+        value={editor.getAttributes('textStyle').color}
+      />
+
       <Select placeholder="Heading Styles" style={{ width: 140 }} onChange={() => headingChange}>
         <Option value="jack">
           <div onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
@@ -68,78 +127,37 @@ const DocMenuBar = ({ editor }) => {
           </div>
         </Option>
         <Option value="jack">
-          <div
-            onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          <div onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
             className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}>
             H2
           </div>
         </Option>
         <Option value="jack">
-          <div
-            onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          <div onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
             className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}>
             H3
           </div>
         </Option>
         <Option value="jack">
-          <div
-            onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+          <div onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
             className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}     >
             H4
           </div>
         </Option>
         <Option value="jack">
-          <div
-            onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+          <div onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
             className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}>
             H5
           </div>
         </Option>
         <Option value="jack">
-          <div
-            onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+          <div onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
             className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}>
             H6
           </div>
         </Option>
       </Select>
-      
-      <div
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
-      >
-        <MdFormatListBulleted/>
-      </div>
-      <div
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
-      >
-        <GrOrderedList/>
-      </div>
-      <div
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? 'is-active' : ''}
-      >
-        code block
-      </div>
-      <div
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive('blockquote') ? 'is-active' : ''}
-      >
-        blockquote
-      </div>
-      <div onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        horizontal rule
-      </div>
-      <div onClick={() => editor.chain().focus().setHardBreak().run()}>
-        hard break
-      </div>
-      <div onClick={() => editor.chain().focus().undo().run()}>
-        undo
-      </div>
-      <div onClick={() => editor.chain().focus().redo().run()}>
-        redo
-      </div>
+
     </div>
   )
 }
